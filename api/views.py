5cdbfa39714a918ef64fbb48b676dcb9cfbb44ba
carlosads1998 <carlos.ads1998@gmail.com>
2022-02-22 14:56:37 -0300

    deploy

--- api/views.py
+++ api/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from .models import funcionario
 from .serializers import funcionarioSerializer
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class funcionarioApiView(generics.ListCreateAPIView):
     queryset = funcionario.objects.all()
     serializer_class = funcionarioSerializer
+    filter_backends= [DjangoFilterBackend]
+    filterset_fields = ['usuario']
 
 class funcionariosApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = funcionario.objects.all()
