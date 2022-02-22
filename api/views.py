1e059daf40cc92bce03f3b3ed03d741e2c83619c
Carlos Alex <64806708+carlosads1998@users.noreply.github.com>
2022-02-22 15:15:15 -0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -6,7 +6,12 @@ from rest_framework import generics
 class funcionarioApiView(generics.ListCreateAPIView):
     queryset = funcionario.objects.all()
     serializer_class = funcionarioSerializer
+    filter_backends= [DjangoFilterBackend]
+    filterset_fields = ['usuario']
+
 
 class funcionariosApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = funcionario.objects.all()
     serializer_class = funcionarioSerializer
+    filter_backends= [DjangoFilterBackend]
+    filterset_fields = ['usuario']
